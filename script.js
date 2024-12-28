8fb4288bcce4c4a828b19baf746a30fe9b5fd961
subratshakya <subratshakya20@gmail.com>
2024-12-28 22:37:17 +0530

    third

--- script.js
+++ script.js
@@ -4,6 +4,20 @@
         // console.log("My name is Subrat Shakya");
         // console.log(1+1);
         //console.log("This is a hello from external JS file")
-        let age=22;
-        console.log(age);
-         
+        // let age=22;
+        // console.log(age);
+        //Numbers
+         let num1=3.14;
+         let num2= 210;
+        console.log(num1+num2);
+        //strings
+        const firstName="Subrat";
+        const lastName="Shakya";
+        console.log(firstName+lastName)
+        //bool
+        let login=true;
+        let signup=false
+        console.log(login)
+        //Null
+        let lastlogin=null;
+        console.log(lastlogin)
