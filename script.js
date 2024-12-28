b844eb0101c6d6f5070778673823461f552815c3
subratshakya <subratshakya20@gmail.com>
2024-12-28 19:52:09 +0530

    second

--- script.js
+++ script.js
@@ -3,5 +3,7 @@
         // console.log("Hello World ");
         // console.log("My name is Subrat Shakya");
         // console.log(1+1);
-        console.log("This is a hello from external JS file")
+        //console.log("This is a hello from external JS file")
+        let age=22;
+        console.log(age);
          
