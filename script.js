219f9bbce44ac5fc799dae5c9df49af1cd2d4542
subratshakya <subratshakya20@gmail.com>
2024-12-28 19:29:26 +0530

    first

---
+++ script.js
@@ -0,0 +1,7 @@
+
+        // //internal linking
+        // console.log("Hello World ");
+        // console.log("My name is Subrat Shakya");
+        // console.log(1+1);
+        console.log("This is a hello from external JS file")
+         
