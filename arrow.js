58adc22dbfb1bca639f74be1d02a1a2db275bebb
subratshakya <subratshakya20@gmail.com>
2024-12-29 19:58:00 +0530

    Arrow

---
+++ arrow.js
@@ -0,0 +1,15 @@
+// const sayHello=()=>{
+//     console.log('Hey');
+// }
+// // sayHello();
+// const addNum=(...nums)=>{
+//     console.log(nums);
+// }
+// addNum(10,20,30,40);
+
+const say=()=>{
+    console.log('Hey');
+}
+
+say()
+
