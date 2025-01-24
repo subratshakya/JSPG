40d3970dc3cf651ca50c0990cbd6a4bf030f7ab7
subratshakya <subratshakya20@gmail.com>
2025-01-25 00:48:06 +0530

    new array

---
+++ array.js
@@ -0,0 +1,6 @@
+const student=["Subrat","subrat1","subrat2","subrat3"]
+console.log(student[0] );
+student.push("Subrat4");
+console.log(student);
+const newarr=[1,2,"car","🍾"];
+newarr.push({name:"Subrat2024"});
---
+++ hig.js
@@ -0,0 +1,109 @@
++91-9837983336
+shakyasubrat80@gmail.com
+Motilal Nehru National Institute of Technology, Allahabad
+LinkedIn: https://www.linkedin.com/in/subratshakya
+GitHub: shakyasubrat
+
+Motilal Nehru National Institute of Technology (MNNIT), Allahabad
+B.Tech. in Mechanical Engineering
+
+To,
+Hiring Manager
+CoinBase
+Dear Hiring Manager,
+
+I am writing to express my interest in the Software Engineering Summer Internship CoinBase. Currently, I am pursuing a B.Tech in Mechanical Engineering at MNNIT Allahabad, and I am eager to apply my technical skills and knowledge to contribute to the innovative projects at your esteemed organization.
+
+Throughout my academic journey, I have worked on several tech stack projects, gaining valuable experience in full-stack development. My projects, including an Expense Tracker and a Book Management System, have enhanced my proficiency in programming languages such as C, C++, and JavaScript. Additionally, I have developed a strong foundation in data structures, algorithms, and database management, I also worked on a ML project based on crop prediction which are crucial skills for excelling in software engineering roles.
+
+I am particularly drawn to CoinBase cause of its commitment to excellence and its emphasis on innovation in technology. The opportunity to work alongside your talented team would allow me to further hone my technical and soft skills, while contributing to impactful projects.
+
+Thank you for considering my application. I am excited about the possibility of learning, contributing, and making a meaningful impact at CoinBase
+
+Sincerely,
+Subrat Shakya
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
